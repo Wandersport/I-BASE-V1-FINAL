aab501933fc06b68c46337ca06596067010874df
Wandersport <83657128+Wandersport@users.noreply.github.com>
2021-09-07 21:05:15 +0200

    Update __resource.lua

--- resources/[esx]/[esx-basics]/esx_compasandstreet/__resource.lua
+++ resources/[esx]/[esx-basics]/esx_compasandstreet/__resource.lua
@@ -1,4 +1,5 @@
-resource_manifest_version "77731fab-63ca-442c-a67b-abc70f28dfa5"
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
 description "MsQuerade's Compass and Streetname HUD"
 version "1.0"
 
